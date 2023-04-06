1c4ddec47da32120309d9f91054a39ea57fc52e6
szebiniso <szebiniso0909@gmail.com>
2023-04-06 06:04:14 +0600

    just testing deploy

--- src/pages/Admins.jsx
+++ src/pages/Admins.jsx
@@ -3,13 +3,11 @@ import HeaderWithOptions from "../shared/ui/HeaderWithOptions";
 import Header from "../shared/ui/Header";
 import Container from "../shared/ui/Container";
 import AdminContent from "../entities/Admin/ui/AdminContent";
-import SideNavbar from "../widgets/sidebar/ui/SideNavbar";
 
 const Admins = () => {
 
   return (
-    <div className="grid grid-cols-5/95">
-      <SideNavbar />
+    <>
       <Container>
         <div className="mx-auto mb-8 w-full lg:mb-12">
           <Header title='Администраторы'/>
@@ -18,7 +16,7 @@ const Admins = () => {
         </div>
         <AdminContent/>
       </Container>
-    </div>
+    </>
   );
 };
 
